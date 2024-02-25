c62d53f612c5e931c8ed21967f498d1b1a234f80
1010nishant <nishantjangid6377@gmail.com>
2024-02-25 17:16:23 +0530

    some changes

--- app.js
+++ app.js
@@ -45,7 +45,56 @@ app.options('*', cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
-app.use(helmet());
+// app.use(helmet());
+const scriptSrcUrls = [
+    'https://unpkg.com/',
+    'https://tile.openstreetmap.org',
+    'https://*.cloudflare.com/',
+    'https://cdnjs.cloudflare.com/ajax/libs/axios/',
+    'https://*.stripe.com',
+    'https:',
+    'data:'
+];
+const styleSrcUrls = [
+    'https://unpkg.com/',
+    'https://tile.openstreetmap.org',
+    'https://fonts.googleapis.com/',
+    'https:'
+];
+const connectSrcUrls = [
+    'https://unpkg.com',
+    'https://tile.openstreetmap.org',
+    'https://*.cloudflare.com/',
+    'http://127.0.0.1:3000'
+];
+const fontSrcUrls = [
+    'fonts.googleapis.com',
+    'fonts.gstatic.com',
+    'https:',
+    'data:'
+];
+const frameSrcUrls = ['https://*.stripe.com'];
+app.use(
+    helmet({ crossOriginResourcePolicy: false, crossOriginEmbedderPolicy: false })
+);
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'", 'data:', 'blob:'],
+            baseUri: ["'self'"],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", 'data:', 'blob:'],
+            objectSrc: ["'none'"],
+            imgSrc: ["'self'", 'blob:', 'data:', 'https:'],
+            fontSrc: ["'self'", ...fontSrcUrls],
+            childSrc: ["'self'", 'blob:'],
+            frameSrc: ["'self'", ...frameSrcUrls],
+            upgradeInsecureRequests: []
+        }
+    })
+);
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,50 +1,29 @@
 /* eslint-disable */
-export const displayMap = locations => {
-    mapboxgl.accessToken =
-        mapboxgl.accessToken = 'pk.eyJ1IjoiMTAxMG5pc2hhbnQiLCJhIjoiY2xyeDZmZ3FvMTdyNTJxbG13YTdoaHpsNiJ9.ASJhM8EFHljf5wJ-uZS8xQ';
-    // mapboxgl.accessToken = 'pk.eyJ1IjoiMTAxMG5pc2hhbnQiLCJhIjoiY2xyeDY0aDFqMTc5MTJvcGJjODE1Y25jdiJ9.jTJ68ySTFZ5fTlr8FAC6WA';
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/1010nishant/clrx8a2ou005001qqbvce31is',
-        scrollZoom: false
-        // center: [-118.113491, 34.111745],
-        // zoom: 10,
-        // interactive: false
+export const displayMap = () => {
+    const locations = JSON.parse(document.getElementById('map').dataset.locations);
+    console.log(locations);
+    // const locationsData = JSON.parse(
+    //     document.getElementById('map').dataset.locations
+    // );
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+    const markerArray = [];
+    locations.forEach((loc) => {
+        const reversedArr = [...loc.coordinates].reverse();
+
+        const myIcon = L.icon({
+            iconUrl: './../img/pin.png',
+            iconSize: [30, 35],
+            iconAnchor: [15, 35],
+        });
+
+        L.marker(reversedArr, { icon: myIcon }).addTo(map);
+        markerArray.push(reversedArr);
     });
+    const bounds = L.latLngBounds(markerArray);
+    map.fitBounds(bounds);
 
-    const bounds = new mapboxgl.LngLatBounds();
-
-    locations.forEach(loc => {
-        // Create marker
-        const el = document.createElement('div');
-        el.className = 'marker';
-
-        // Add marker
-        new mapboxgl.Marker({
-            element: el,
-            anchor: 'bottom'
-        })
-            .setLngLat(loc.coordinates)
-            .addTo(map);
-
-        // Add popup
-        new mapboxgl.Popup({
-            offset: 30
-        })
-            .setLngLat(loc.coordinates)
-            .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-            .addTo(map);
-
-        // Extend map bounds to include current location
-        bounds.extend(loc.coordinates);
-    });
-
-    map.fitBounds(bounds, {
-        padding: {
-            top: 200,
-            bottom: 150,
-            left: 100,
-            right: 100
-        }
-    });
 };
